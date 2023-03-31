124ca71cc64cafc497e54449c8dbdfad727475f9
KimYongH <127469614+KimYongH@users.noreply.github.com>
2023-03-31 15:09:38 +0900

    dao
    dao 수정

--- src/main/java/com/team/biz/dto/FavoriteVO.java
+++ src/main/java/com/team/biz/dto/FavoriteVO.java
@@ -1,5 +1,18 @@
 package com.team.biz.dto;
 
+import java.util.Date;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public class FavoriteVO {
+	
+	private String id;
+	private int product_no;
+	private Date favorite_regdate;
 
 }
