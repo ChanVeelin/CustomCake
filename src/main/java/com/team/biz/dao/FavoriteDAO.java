5fa51ba832b0ca17de996d2b39de10fc27f839e4
KimYongH <127469614+KimYongH@users.noreply.github.com>
2023-03-31 15:54:36 +0900

    favorite
    추가

--- src/main/java/com/team/biz/dao/FavoriteDAO.java
+++ src/main/java/com/team/biz/dao/FavoriteDAO.java
@@ -1,8 +1,12 @@
 package com.team.biz.dao;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+
+import com.team.biz.dto.FavoriteVO;
 
 @Repository
 public class FavoriteDAO {
@@ -10,5 +14,17 @@ public class FavoriteDAO {
 	@Autowired
 	private SqlSessionTemplate mybatis;
 	
+	public void insertFavorite() {
+		mybatis.insert("FavoriteMapper.insertFavorite");
+	}
+	
+	public List<FavoriteVO> getListByFavorite(FavoriteVO vo){
+	   
+		return mybatis.selectList("FavoriteMapper.getListByFavorite",vo);
+	}
+	
+	public void deleteByFavorite(FavoriteVO vo) {
+		  mybatis.delete("FavoriteMapper.deleteByFavorite",vo);
+	}
 	
 }
